4af4db5e3f51592b9c7d9bbd5e476bceebed76ca
Havok <Christian.Alejandro.Guzman@gmail.com>
2018-08-26 10:40:56 -0300

    Logging finished

---
+++ Comical/Comical.Repository/HistoryExceptionRepository.cs
@@ -0,0 +1,55 @@
+﻿using Comical.Models;
+using Comical.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comical.Repository
+{
+    public class HistoryExceptionRepository : BaseRepository
+    {
+        public int New(HistoryException model)
+        {
+            var output = this.UnitOfWork.ScalarDirect(
+                "HistoryException_new",
+                ParametersBuilder.With("Section", model.Section)
+                .And("ExceptionType", model.ExceptionType)
+                .And("ExceptionSource", model.ExceptionSource)
+                .And("ExceptionMessage", model.ExceptionMessage)
+                .And("ExceptionStackTrace", model.ExceptionStackTrace)
+                .And("UserId", model.UserId)
+            ).AsInt();
+
+            return output;
+        }
+
+        internal HistoryException FetchModel(IDataReader reader)
+        {
+            var item = new HistoryException
+            {
+                Id = reader.GetInt32("Id"),
+                Section = reader.GetString("Section"),
+                ExceptionType = reader.GetString("ExceptionType"),
+                ExceptionSource = reader.GetString("ExceptionSource"),
+                ExceptionMessage = reader.GetString("ExceptionMessage"),
+                ExceptionStackTrace = reader.GetString("ExceptionStackTrace"),
+                UserId = reader.GetInt32Nullable("UserId"),
+                DateLogged = reader.GetDateTime("DateLogged")
+            };
+
+            return item;
+        }
+
+        public IEnumerable<HistoryException> Get()
+        {
+            var output = this.UnitOfWork.GetDirect(
+                "HistoryException_get",
+                this.FetchModel);
+
+            return output;
+        }
+    }
+}
---
+++ Comical/Comical.Services/LoggingService.cs
@@ -0,0 +1,65 @@
+﻿using Comical.Models;
+using Comical.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comical.Services
+{
+    public class LoggingService
+    {
+
+        #region Thread-safe Singleton
+
+        private LoggingService() { }
+        static LoggingService() { }
+
+        private static readonly LoggingService instance = new LoggingService();
+
+        public static LoggingService obj => instance;
+
+        #endregion
+
+        private Lazy<ISessionService> lazySessionService = new Lazy<ISessionService>(() => DependencyResolver.obj.Resolve<ISessionService>());
+
+        private ISessionService SessionService => lazySessionService.Value;
+
+        public void Log(string section, string message)
+        {
+            var repository = new HistoryEventRepository();
+            var userId = this.SessionService.CurrentUserId;
+
+            var item = new HistoryEvent
+            {
+                Section = section,
+                Message = message,
+                UserId = userId
+            };
+
+            repository.New(item);
+        }
+
+        public void Log(string section, Exception ex)
+        {
+            var repository = new HistoryExceptionRepository();
+            var userId = this.SessionService.CurrentUserId;
+
+            var item = new HistoryException
+            {
+                Section = section,
+                ExceptionType = ex.GetType().FullName,
+                ExceptionSource = ex.Source,
+                ExceptionMessage = ex.Message,
+                ExceptionStackTrace = ex.StackTrace,
+                UserId = userId
+            };
+
+            repository.New(item);
+
+            if (ex.InnerException != null) this.Log(section, ex.InnerException);
+        }
+
+    }
+}
