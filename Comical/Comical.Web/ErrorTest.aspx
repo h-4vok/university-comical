deec95089f3e3f0a5f1e2ecea5ed9b0028347285
Christian  Guzman <cguzman@resolvit.com>
2018-10-30 21:37:03 -0300

    Added an error test page to generate exceptions

---
+++ Comical/Comical.Web/ErrorTest.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Prueba de Error" 
+    Language="C#" 
+    MasterPageFile="~/Site.Master" 
+    AutoEventWireup="true" 
+    CodeBehind="ErrorTest.aspx.cs" 
+    Inherits="Comical.Web.ErrorTest" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2 class="text-center"><%: Title %></h2>
+    
+</asp:Content>
---
+++ Comical/Comical.Web/ErrorTest.aspx.cs
@@ -0,0 +1,21 @@
+﻿using Comical.Models;
+using Comical.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Comical.Web
+{
+    public partial class ErrorTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            throw new SystemException("Este es un error de prueba global.");
+        }
+    }
+}
